8c08885c74be8e0c201b8a3abba03921c57041ab
rupakshi11 <rupakshi.11@gmail.com>
2020-02-27 01:24:11 -0600

    test commit 3

--- Assignments/src/main/java/testCases/Assign02.java
+++ Assignments/src/main/java/testCases/Assign02.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
-//Rupakshi
+
 public class Assign02 {
 
 	WebDriver driver = null;
